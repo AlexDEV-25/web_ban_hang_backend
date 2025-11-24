46d98461b1c7e40c06eb67eec67fccf815c6c459
“alex” <hoangvanduc2504@gmail.com>
2025-11-24 18:20:20 +0700

    them 2 cau truy van de tao endpoint

--- src/main/java/com/example/app/repository/ProductRepository.java
+++ src/main/java/com/example/app/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.example.app.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -7,5 +9,9 @@ import com.example.app.entity.Product;
 
 @RepositoryRestResource(path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	Page<Product> findByProductNameContaining(String productName, Pageable pageable);
+
+	Page<Product> findByCategories_Id(Long categoryId, Pageable pageable);
 
 }
